63e77c9995e0fd0d83369bedd4e0f917b9343b4d
Laura Pereda <lypereda17@gmail.com>
2026-03-04 17:43:00 -0600

    Created next.js pg that lets user add dishes with name, unit and cost

--- app/dishes/page.tsx
+++ app/dishes/page.tsx
@@ -1,3 +1,86 @@
+'use client'
+
+import { useState } from 'react';
+
 export default function Dishes(){
-    return(<h1>Dishes</h1>)
+    
+    const [dishName, setDishName] = useState("");
+    const [unitType, setUnitType] = useState("plate");
+    const [baselineCostPerUnit, setbaselineCostPerUnit] = useState("");
+    const [dishes, setDishes] = useState([]);
+    const listDishes = dishes.map(dish => 
+    <li key={dish.dishName}>
+        <p>
+            <b>{dish.dishName}</b> {' '}
+            <b>{dish.unitType}</b> {' '}
+            <b>{dish.baselineCostPerUnit}</b> {' '}
+        </p>
+    </li>)
+
+    function handleClick(){
+        const costNum = Number(baselineCostPerUnit);
+        if(costNum > 0 && dishName.trim() !== ""){
+            const newDish = {dishName: dishName, unitType: unitType, baselineCostPerUnit: costNum};
+            setDishes(prev => [...prev, newDish]);
+            setDishName("");
+            setbaselineCostPerUnit("");
+        }
+    }
+    
+    function handleDishNameChange(e){
+        setDishName(e.target.value);
+    }
+
+    function handleBaselineCostChange(e){
+        setbaselineCostPerUnit(e.target.value);
+    }
+
+    return(
+    <>
+        <h1>Dishes</h1>
+        <p>
+            
+           <label>
+            Input Dish Name:{' '}
+            <input
+                value={dishName}
+                onChange={handleDishNameChange}
+            />
+        </label> 
+        </p>
+        
+        <p>
+            <label>
+                Select Unit Option {' '}
+                <select value={unitType} onChange={e => setUnitType(e.target.value)}>
+                    <option value="tray">Tray</option>
+                    <option value="plate">Plate</option>
+                </select>
+            </label>
+        </p>
+        
+        <p>
+            <label>
+                Input Baseline Cost:{' '}
+                <input
+                    type="number"
+                    value={baselineCostPerUnit}
+                    onChange={handleBaselineCostChange}
+                />
+            </label>
+        </p>
+        <p>
+            <button onClick={handleClick}>
+                Add Dish
+            </button>
+        </p>
+        
+        Inputted DishName: {dishName} {' '}
+        {' '} Selected Unit: {unitType}
+        {' '} Inputted Cost: {baselineCostPerUnit}
+        {' '} Current Dishes count: {dishes.length}
+        {' '} List of dishes: <ul>{listDishes}</ul>
+        
+    </>
+)
 };
