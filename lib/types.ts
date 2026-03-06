a4f988df83fb5d0dc5a78951afbf5e768e2d8de7
Laura Pereda <lypereda17@gmail.com>
2026-03-05 18:54:20 -0600

    Created a type Quote

--- lib/types.ts
+++ lib/types.ts
@@ -4,3 +4,17 @@ export type Dish = {
     unitType: UnitType;
     baselineCostPerUnit: number;
 }
+
+export type Quote = {
+    id: string;
+    savedAt: string;
+    dishName: string;
+    quantity: number;
+    pricePerUnit: number;
+    grocerySpend: number;
+    laborHours: number;
+    marginPct: number;
+    totalCost: number;
+    renenue: number;
+    profit: number;
+}
