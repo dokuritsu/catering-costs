7c9ef879c842fa240720cc7851cb9dc4a3c52448
Laura Pereda <lypereda17@gmail.com>
2026-03-13 16:21:03 -0500

    Created POST in dishes. Contains desconstructing request body, validation of body and insert call to supabase

--- app/api/dishes/route.ts
+++ app/api/dishes/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { supabase } from "@/lib/supabase";
+import { Dish } from "@/lib/types";
 
 export async function GET() {
     const { data, error } = await supabase.from("dishes").select("*").order("created_at", { ascending: false});
@@ -9,4 +10,22 @@ export async function GET() {
     }
 
     return NextResponse.json(data, {status: 200});
+}
+
+export async function POST(request: Request){
+    const body = await request.json();
+    const {dishName, unitType, baselineCostPerUnit} = body;
+
+    if(!dishName || !unitType || baselineCostPerUnit <= 0){
+        return NextResponse.json({error: "Bad request sent"}, {status: 400});
+    }
+
+    const {data, error} = await supabase.from("dishes").insert([{dish_name: dishName, unit_type: unitType, baseline_cost_per_unit: baselineCostPerUnit}]);
+        
+    if(error){
+        return NextResponse.json({error: error.message}, {status: 500});
+    }
+
+    return NextResponse.json(data, {status:201});
+
 }
